bf8a3dc967b85b0b6394c6b81406e1711f76e2f0
Richard Baltrusch <richard@baltrusch.net>
2022-03-18 21:35:04 +0100

    Update run_tests

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,29 +1,10 @@
 ::runs batest.bat in the batest folder. Used for a Github Actions Workflow.
-::Note that batest does not and cannot return errorlevel if a test failed (windows bug)
-::Instead, this is solved by echoing out the number of failed tests and redirecting this.
-
 @echo off
 setlocal
-set testdir=..\..\src
-set tempfile=__batestresult__
-
 ::Temporarily add objectbatch to path
 set path=%~dp0../objectbatch/objectbatch;%path%
 
 ::change here so assert.bat is on path
 cd %~dp0../batest/batest
-call batest %testdir% > %tempfile%
-
-::call batest again in verbose mode to print all messages
-call batest -v -n %testdir%
-echo.
-
-pushd %testdir%
-type test_report.html
-popd
-
-echo.
-set /p ERRORLEVEL= < %tempfile%
-del %tempfile%
-echo %ERRORLEVEL% error(s) occured.
-exit /b %ERRORLEVEL%
+call batest -v -n -t ..\..\src
+exit /b %errorlevel%
